5c16983684b08768da1654e3cccf4f0f8b1af681
elsa <449134764@qq.com>
2019-07-17 23:38:01 -0500

    Admin posts

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(blank=True, null = True)
+    image = ProcessedImageField(
+        upload_to='statics/images/posts',
+        # processors=[ResizeToFill(100, 50)],
+        format='JPEG',
+        options={'quality': 100},
+        blank=True,
+        null=True,
+     )
+    def __str__(self):
+         return self.title
